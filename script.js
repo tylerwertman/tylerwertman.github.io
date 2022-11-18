2e29d351285c42b1f78f2ba40cdd3b8670f7cdec
Tyler <tylerwertman@outlook.com>
2022-11-17 23:44:41 -0500

    nav bar and icons

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("websiteLoaded");
+
+function navUlColor(element) {
+    console.log("navUlColor running");
+    var li = element;
+    var name = li.children[1].innerText;
+    console.log(name);
+    console.log(li);
+    if (!li.classList.contains("navLiColor")) {
+        li.classList.add("navLiColor");
+        li.children[0].src = `./icons/${name}-fill.svg`
+        li.children[1].style.color = "white";
+    }else {
+        li.classList.remove("navLiColor");
+        li.children[0].src = `./icons/${name}.svg`
+        li.children[1].style.color = "darkslategrey";
+    }
+}
+function navH1Color(element) {
+    console.log("navH1Color running");
+    var h1 = element;
+    console.log(h1);
+    if (!h1.classList.contains("navH1Color")) {
+        h1.classList.add("navH1Color");
+    }else {
+        h1.classList.remove("navH1Color");
+    }
+}
