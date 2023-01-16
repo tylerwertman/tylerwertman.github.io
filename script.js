aa72402c7c53c3752a0e79980c77080a2bce139c
Tyler <tylerwertman@outlook.com>
2023-01-16 17:50:33 -0500

    fix navbar, add edu, todo

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ console.log("websiteLoaded");
 function navH1Hover(element) {
     console.log("navH1Hover running");
     var h1 = element;
-    console.log(h1);
-    if(!h1.classList.contains("navH1Hover")) {
-        h1.classList.add("navH1Hover");
-        h1.style.color = "seagreen";
+    console.log(h1.children[0].innerText);
+    if(!h1.children[0].classList.contains("navH1Hover")) {
+        h1.children[0].classList.add("navH1Hover");
+        h1.children[0].style.color = "seagreen";
     }else{
-        h1.classList.remove("navH1Hover");
-        h1.style.color = "darkslategrey";
+        h1.children[0].classList.remove("navH1Hover");
+        h1.children[0].style.color = "darkslategrey";
     }
 }
 
 function navLiHover(element) {
     console.log("navLiHover running");
     var li = element;
-    var name = li.children[1].innerText;
-    // console.log(name);
-    // console.log(li);
-    if (!li.classList.contains("navLiHover")) {
-        li.classList.add("navLiHover");
-        li.children[0].src = `./images/${name}-fill-SG.svg`
+    var name = li.children[0].children[0];
+    console.log(li);
+    console.log(name);
+    if (!name.classList.contains("navLiHover")) {
+        name.classList.add("navLiHover");
+        name.src = `./images/${name.alt}-fill-SG.svg`
         // console.log(li.children[0].src);
-        li.children[1].style.color = "seagreen";
+        li.children[0].style.color = "seagreen";
     }else {
-        li.classList.remove("navLiHover");
-        li.children[0].src = `./images/${name}-DSG.svg`
-        li.children[1].style.color = "darkslategrey";
+        name.classList.remove("navLiHover");
+        name.src = `./images/${name.alt}-DSG.svg`
+        li.children[0].style.color = "darkslategrey";
     }
 }
 function buttonHover(element) {
